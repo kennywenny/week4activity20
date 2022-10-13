80a47b56d27bc7f9b27f0404d8a9c70c26f390c0
Kenny Hung (he) <khung@instructors.2u.com>
2022-10-13 21:09:09 +0100

    Remove temp code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,11 @@
-var container = document.querySelector(".container");
+const container = document.querySelector(".container");
 
 container.addEventListener("click", function(event) {
-  var element = event.target;
+  const element = event.target;
   if (!element.matches('.box')) {
-    console.log('Did not click on box')
     return
   }
   const dataNumber = element.getAttribute('data-number')
   // const alternativeDataNumber = element.dataset.number
-  console.log('Did click on box')
   element.textContent = dataNumber
 });
