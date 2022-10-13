053efded0580b471e2cadb16260c0d5af6027546
Kenny Hung (he) <khung@instructors.2u.com>
2022-10-13 21:03:05 +0100

    Pseudocoding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
 var container = document.querySelector(".container");
 
 container.addEventListener("click", function(event) {
+  console.log('Hello mum')
   var element = event.target;
 
+  // if element is not class="box", then do nothing
+  // if the data state is hidden:
+  //   * make it unhidden
+  //   * make the contents of the element equal to the data-number
   // TODO: Complete function
 });
