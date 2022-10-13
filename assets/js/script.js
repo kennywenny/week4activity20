d05c2b2b855009ba412603bcaae6645ac0d25c73
Kenny Hung (he) <khung@instructors.2u.com>
2022-10-13 21:07:35 +0100

    Get data-number attribute on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
 var container = document.querySelector(".container");
 
 container.addEventListener("click", function(event) {
-  console.log('Hello mum')
   var element = event.target;
+  if (!element.matches('.box')) {
+    console.log('Did not click on box')
+    return
+  }
+  const dataNumber = element.getAttribute('data-number')
+  // const alternativeDataNumber = element.dataset.number
+  console.log('Did click on box')
 
   // if element is not class="box", then do nothing
   // if the data state is hidden:
